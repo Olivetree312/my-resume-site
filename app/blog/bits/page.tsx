a0a1e743ff02affa6e961848b530d9cf83da2a3c
Olivia Chen <chenhuiya2005@gmail.com>
2026-01-14 14:10:22 -0800

    added froglife site

--- app/blog/bits/page.tsx
+++ app/blog/bits/page.tsx
@@ -4,22 +4,23 @@ export default function BitsPage() {
   return (
     <section>
       <h1 className="text-xl font-bold mb-4">✨Cool Bits and Bobs</h1>
-      <p>~Shiny things...MY PRECIOUS</p>
+      <p>~Shiny things I found</p>
 
       <section className="max-w-3xl mx-auto p-6 bg-yellow-50 rounded-2xl shadow-lg">
         <h1 className="text-4xl font-bold text-yellow-900 mb-4">mini shrines to my fav things</h1>
         <div className="space-y-6">
 
           <div className="p-4 bg-white rounded-xl border border-yellow-200">
-            <h2 className="text-xl font-semibold text-yellow-900"> 🐸 a blog about frogs—a frog blog</h2>
+            <h2 className="text-xl font-semibold text-yellow-900"> 🐸 a site all about frogs</h2>
             <p className="text-yellow-800 mb-2">
             green, slimy, and beautiful!
             </p>
             <a
-              href="/frog-blog/"
+              href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.froglife.org/&ved=2ahUKEwjV3JKUhYySAxW6DkQIHXijIpoQFnoECBsQAQ&usg=AOvVaw0zn13ZjwwfrAzUlXQhhivT"
+              target ="_blank"
               className="text-amber-700 underline hover:text-amber-900"
             >
-              FrogBlog
+              Froglife
             </a>
           </div>
         </div>
